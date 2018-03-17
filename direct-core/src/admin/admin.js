5c5d38a057b822d946c08c9305b1c9cae80f8a68
Jacob McCrae <jacobmccrae@Jacobs-MacBook-Pro.local>
2018-03-17 15:16:22 -0700

    Admin: Created the static view of the needs categories for selection in the top navbar.

--- direct-core/src/admin/admin.js
+++ direct-core/src/admin/admin.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
 
+function ListItem(props){
+    return (
+        <li><span className={props.value.toLowerCase()}><i className={props.icon}></i></span> {props.value}</li>
+    );
+}
+
+function NeedsList(props){
+    const needs = props.needs;
+    const listItems = needs.map((need) => 
+        <ListItem key={need.id} value={need.title} icon={need.icon} />
+    );
+    return (
+        <ul>{listItems}</ul>
+    );
+}
+
+const needs = [
+    {id: 1, title: 'Physiological', icon: 'fas fa-medkit', color: ''}, 
+    {id: 2, title: 'Safety', icon: 'fas fa-medkit', color: ''}, 
+    {id: 3, title: 'Belonging', icon: 'fas fa-medkit', color: ''}, 
+    {id: 4, title: 'Esteem', icon: 'fas fa-medkit', color: ''}, 
+    {id: 5, title: 'Self-Actualization', icon: 'fas fa-medkit', color: ''} 
+];
+
 class Admin extends Component {
   render() {
     return (
-      <div className="admin">
-       <div className="admin-navbar">
-        Here navbar
-       </div>
-       <div className="admin-main-view">
-        Here mainview
-       </div>
-      </div>
+        <div className="admin">
+            <div className="admin_header">
+                <div className="header">Core</div>
+            </div>
+            <div className="admin_navbar">
+                <div className="needs_list">
+                    <NeedsList needs={needs} />
+                </div>
+                <div className="percentile_slider">
+                    Slider Goes Here
+                </div>
+            </div>
+            <div className="admin_main_view">
+                Here mainview
+            </div>
+        </div>
     )
   }
 }
