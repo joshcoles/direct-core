b43a40a3f461495f213b964ba45ff838b92cf491
Jacob McCrae <jacob.g.mccrae@gmail.com>
2018-03-17 21:14:09 -0700

    Made the static ugly version of the AdminUser view.

--- direct-core/src/admin-user/admin-user.js
+++ direct-core/src/admin-user/admin-user.js
@@ -13,9 +13,57 @@ class AdminUser extends Component {
   render() {
     return ( 
         <div className="admin_user">
-            <div className="admin_user_view">
+            <div className="col-xs-9 admin_user_view">
+                <div className="stat sub_need_1">
+                    <div className="stat_level">
+                    </div>
+                    <div className="stat_label">
+                        Air
+                    </div>
+                </div>
+                <div className="stat sub_need_2">
+                    <div className="stat_level">
+                    </div>
+                    <div className="stat_label">
+                        Water
+                    </div>
+                </div>
+                <div className="stat sub_need_3">
+                    <div className="stat_level">
+                    </div>
+                    <div className="stat_label">
+                        Food
+                    </div>
+                </div>
+                <div className="stat sub_need_4">
+                    <div className="stat_level">
+                    </div>
+                    <div className="stat_label">
+                        Shelter
+                    </div>
+                </div>
+                <div className="stat sub_need_5">
+                    <div className="stat_level">
+                    </div>
+                    <div className="stat_label">
+                        Sleep
+                    </div>
+                </div>
             </div>
-            <div className="admin_user_navbar">
+            <div className="col-xs-3 admin_user_navbar">
+                <div className="user_info">
+                    <div className="user_avatar">
+                        <img src="https://placeimg.com/200/200/people/grayscale" />
+                        <div className="user_stat">
+                        </div>
+                    </div>
+                    <div className="user_name">
+                        Alex Hover
+                    </div>
+                    <div className="user_age">
+                        30 YEARS OLD
+                    </div>
+                </div>
                 <div className="needs_list">
                     <NeedsList needs={needs} />
                 </div>
