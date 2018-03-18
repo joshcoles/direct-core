a42564b38cd028eeea63677c3e43b63fdad93212
Mohsen <mohkami.mohsen@gmail.com>
2018-03-18 00:33:49 -0700

    including asset files

---
+++ direct-core/src/constants/needs.js
@@ -0,0 +1,56 @@
+export const NEEDS_ITEMS = [
+  {
+    id: 1,
+    title: 'Physiological',
+    items: ["Air", "Water", "Food", "Sleep", "Reproduction"],
+    icon: '/assets/physiological.png'
+  },
+  {
+    id: 2,
+    title: 'Belonging',
+    items: ["Friendship", "Intimacy", "Family", "Connection"],
+    icon: '/assets/belonging.png'
+  },
+  {
+    id: 3,
+    title: 'Safety',
+    items: ["Friendship", "Intimacy", "Family", "Connection"],
+    icon: '/assets/safety.png'
+  },
+  {
+    id: 4,
+    title: 'Esteem',
+    items: ["Friendship", "Intimacy", "Family", "Connection"],
+    icon: '/assets/esteem.png'
+  },
+  {
+    id: 5,
+    title: 'Self Desire',
+    items: ["Friendship", "Intimacy", "Family", "Connection"],
+    icon: '/assets/self_desire.png'
+  }
+]
+
+export const Needs = {
+  physiological: {
+    id: 1,
+    title: 'Physiological',
+    items: ["Air", "Water", "Food", "Sleep", "Reproduction"]
+  },
+  belonging: {
+    title: 'Belonging',
+    items: ["Friendship", "Intimacy", "Family", "Connection"]
+  },
+  safety: {
+    title: 'Safety',
+    items: ["Friendship", "Intimacy", "Family", "Connection"]
+  },
+  esteem: {
+    title: 'Esteem',
+    items: ["Friendship", "Intimacy", "Family", "Connection"]
+  },
+  selfDesire: {
+    title: 'Self Desire',
+    items: ["Friendship", "Intimacy", "Family", "Connection"]
+  }
+}
