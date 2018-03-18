d6791d2e5ecc2a6733f55972232bc7e90e52b1e3
Mohsen <mohkami.mohsen@gmail.com>
2018-03-17 20:39:16 -0700

    color the slider background based on its value

--- direct-core/src/admin/state-slider/state-slider.js
+++ direct-core/src/admin/state-slider/state-slider.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Slider from 'react-rangeslider'
 
 class Horizontal extends Component {
   constructor (props, context) {
     super(props, context)
     this.state = {
-      value: 10
+      value: 50
     }
   }
 
 
   handleChange = value => {
+    this.setSliderBackgroundColor()
     this.setState({
       value: value
     })
     this.props.changeHandler(value)
   };
 
+  componentDidMount() {
+    this.setSliderBackgroundColor()
+  }
+
+  setSliderBackgroundColor() {
+    const { value } = this.state
+
+    let leftSection = ReactDOM.findDOMNode(this.refs.slider);
+    let backgroundColor = "green"
+    if (value < 33) {
+      backgroundColor = "red"
+    }
+    else if (value < 66) {
+      backgroundColor = "orange"
+    }
+    leftSection.firstElementChild.style.backgroundColor = backgroundColor
+  }
 
   render () {
     const { value } = this.state
+
     return (
       <div className='slider'>
         <Slider
+          ref = "slider"
           min={0}
           max={100}
           value={value}
