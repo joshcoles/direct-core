e91b968518a86561e7d041ade44c931b314ca380
Josh Coles <josh@joshcoles.com>
2018-03-24 18:14:00 -0700

    Pulled Admin down to josh's local machine.

--- direct-core/src/App.js
+++ direct-core/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import AdminUser from './admin-user/admin-user';
-
+import Admin from './admin/admin';
 import './App.css';
 
 class App extends Component {
